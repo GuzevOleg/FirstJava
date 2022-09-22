dbd701a4f925e26871ad77af91acefe102ae120d
GuzevOleg <109841909+GuzevOleg@users.noreply.github.com>
2022-09-22 21:56:01 +0400

    Добавил русский язык

--- FirstJava/src/main/FirstJava.java
+++ FirstJava/src/main/FirstJava.java
@@ -3,6 +3,12 @@ package main;
 public class FirstJava {
     public static void main(String[] args) {
         System.out.println("Hello Oleg!");
+
+        String мечта = "быть";
+        String мечта2 = "Стать";
+        System.out.println("Хочу " + мечта + " Автоматизатором!" );
+        System.out.println("Хочу " + мечта2 + " Автоматизатором!" );
+
     }
 }
 
